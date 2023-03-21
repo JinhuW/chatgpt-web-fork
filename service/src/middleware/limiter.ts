3d58480d719fe9155e209d5f7204ab9b0ad2065d
Jinhu Wang <jinhuwang1127@gmail.com>
2023-03-20 21:24:54 -0400

    update rate limiting error msg

--- service/src/middleware/limiter.ts
+++ service/src/middleware/limiter.ts
@@ -3,11 +3,17 @@ import { isNotEmptyString } from '../utils/is'
 
 const MAX_REQUEST_PERHOUR = process.env.MAX_REQUEST_PERHOUR
 
+const rateLimitingError = {
+  message: '[InternalAPI] 请求数量超过20条/小时 | Reach the rate limiting in the past hour',
+  data: null,
+  status: 'Fail',
+}
+
 const limiter = isNotEmptyString(MAX_REQUEST_PERHOUR)
   ? rateLimit({
     windowMs: 60 * 60 * 1000, // 1 小时内最多访问次数
     max: parseInt(MAX_REQUEST_PERHOUR), // 最大请求数,
-    message: 'Too many request from this IP in 1 hour',
+    handler: (request, response, next, options) => response.status(200).send(rateLimitingError),
   })
   : rateLimit({
     windowMs: 60 * 60 * 1000, // 1 小时内最多访问次数
